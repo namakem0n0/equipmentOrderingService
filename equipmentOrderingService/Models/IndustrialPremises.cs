84e6b2cb81a57a59a5f09855726ef4cb1425ffe3
Rostyslav <rrrostyk@gmail.com>
2023-02-24 13:34:48 +0200

    gitignore added

---
+++ equipmentOrderingService/Models/IndustrialPremises.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace equipmentOrderingService.Models
+{
+    public class IndustrialPremises
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public double Area { get; set; }
+    }
+}
---
+++ equipmentOrderingService/Models/TechnicalEquipment.cs
@@ -0,0 +1,9 @@
+﻿namespace equipmentOrderingService.Models
+{
+    public class TechnicalEquipment
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public double Area { get; set; }
+    }
+}
