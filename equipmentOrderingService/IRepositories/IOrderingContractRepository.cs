586a9dfadba9a498707b5c0d0289f71b1b59ee89
Rostyslav <rrrostyk@gmail.com>
2023-02-26 02:24:17 +0200

    ApiKey autorisation.

--- equipmentOrderingService/IRepositories/IOrderingContractRepository.cs
+++ equipmentOrderingService/IRepositories/IOrderingContractRepository.cs
@@ -4,6 +4,5 @@ namespace equipmentOrderingService.IRepositories
 {
     public interface IOrderingContractRepository : IGenericRepository<OrderingContract>
     {
-        //Task<OrderingContract> GetContractWithDetailsAsync(Guid ContractId);
     }
 }
